f68810c3e2b65ec2bf7a390a952b8d7c5a575761
cooper <12110517@mail.sustech.edu.cn>
2024-11-04 07:28:58 +0000

    prepare data for incremental learning

--- mgtbench/loading/dataloader.py
+++ mgtbench/loading/dataloader.py
@@ -3,8 +3,10 @@ import datasets
 import tqdm
 import pandas as pd
 import re
+import copy
 import os
 import json
+from concurrent.futures import ThreadPoolExecutor
 
 # you can add more datasets here and write your own dataset parsing function
 DATASETS = ['TruthfulQA', 'SQuAD1', 'NarrativeQA', "Essay", "Reuters", "WP"]
@@ -294,6 +296,16 @@ def load(name, detectLLM, category='Art', seed=0):
     else:
         raise ValueError(f'Unknown dataset {name}')
 
+
+def download_data(model_name, category):
+    return datasets.load_dataset(
+        "AITextDetect/AI_Polish_clean",
+        trust_remote_code=True,
+        name=model_name,
+        split=category,
+        # cache_dir=cache_dir
+    )
+
 def prepare_attribution(category='Art', seed=0):
     setup_seed(seed)
     # human
@@ -301,9 +313,14 @@ def prepare_attribution(category='Art', seed=0):
 
     # Prepare attribution data 
     model_data = {}
-    for m in MODELS:
-        model_data[m] = datasets.load_dataset("AITextDetect/AI_Polish_clean", trust_remote_code=True, name=m, split=category)
-        # print(m, len(model_data[m]))
+    with ThreadPoolExecutor() as executor:
+        futures = {executor.submit(download_data, m, category): m for m in MODELS}
+        for future in futures:
+            model_name = futures[future]
+            try:
+                model_data[model_name] = future.result()
+            except Exception as e:
+                print(f"Failed to load {model_name}: {e}")
 
     min_len = int(1e9)
     for m in MODELS:
@@ -359,6 +376,133 @@ def load_attribution(category):
         os.makedirs("data")
     if not os.path.exists(saved_data_path):
         data = prepare_attribution(category, seed=3407)
+        with open(saved_data_path, 'w') as f:
+            json.dump(data, f)
+    else:
+        with open(saved_data_path, 'r') as f:
+            data = json.load(f)
+
+    return data
+
+
+def prepare_incremental(order: list, category='Art', seed=0):
+    '''
+    Prepare incremental data for the given category containing 6 models and human data,
+    the order of each model is given by the order list
+    order: ['Moonshot', 'gpt35', 'Llama3'] for example
+    '''
+    setup_seed(seed)
+    # human data
+    subject_human_data = datasets.load_dataset("AITextDetect/AI_Polish_clean", trust_remote_code=True, name='Human', split=category, cache_dir='/data1/zzy/cache/huggingface')
+
+    # models data
+    model_data = {}
+    with ThreadPoolExecutor() as executor:
+        futures = {executor.submit(download_data, m, category, '/data1/zzy/cache/huggingface'): m for m in MODELS}
+        for future in futures:
+            model_name = futures[future]
+            try:
+                model_data[model_name] = future.result()
+            except Exception as e:
+                print(f"Failed to load {model_name}: {e}")
+
+    # TODO: now contrain the length of data to be the same
+    min_len = int(1e9)
+    for m in MODELS:
+        min_len = min(min_len, len(model_data[m]))
+
+    for m in MODELS:
+        model_data[m] = model_data[m].shuffle().select(range(min_len))
+
+    human_sample = subject_human_data.shuffle().select(range(min_len))
+
+    split = 0.8
+    train_data = {}
+    test_data = {}
+    for m in MODELS:
+        train_data[m] = model_data[m].select(range(int(min_len * split)))
+        test_data[m] = model_data[m].select(range(int(min_len * split), min_len))
+
+    train_data['Human'] = human_sample.select(range(int(min_len * split)))
+    test_data['Human'] = human_sample.select(range(int(min_len * split), min_len))
+
+    # label_mapping = {'Human': 0, 'Moonshot': 1, 'gpt35': 2, 'Mixtral': 3, 'Llama3': 4, 'gpt-4omini': 5}
+    num_class = len(order)
+
+    # a list of data, len(train) == len(order), for each round of incremental learning
+    data = {
+        'train': [],
+        'test': []
+    }
+    for i in range(num_class):
+        if i == 0:
+            # for train data, first data should contain shuffled human AND one model data
+            this_model_data = train_data[order[i]]
+            temp = {
+                'text': [],
+                'label': []
+            }
+            data_list = []
+            for d in this_model_data:
+                data_list.append({'text': d['text'], 'label': i + 1})
+            # TODO: here all human data is added to the first data, maybe we can add human data to each stage (as mitigation)
+            for d in train_data['Human']:
+                data_list.append({'text': d['text'], 'label': 0})
+
+            random.shuffle(data_list)
+            for d in data_list:
+                temp['text'].append(d['text'])
+                temp['label'].append(d['label'])
+
+            data['train'].append(temp)
+            # for test data: no need to shuffle
+            temp = {
+                'text': [],
+                'label': []
+            }
+            for d in test_data[order[i]]:
+                temp['text'].append(d['text'])
+                temp['label'].append(i + 1)
+            for d in test_data['Human']:
+                temp['text'].append(d['text'])
+                temp['label'].append(0)
+
+            data['test'].append(temp)
+        else:
+            # for train: the rest of the data contains only one model data
+            this_model_data = train_data[order[i]]
+            temp = {
+                'text': [],
+                'label': []
+            }
+            for d in this_model_data:
+                temp['text'].append(d['text'])
+                temp['label'].append(i + 1)
+
+            data['train'].append(temp)
+
+            # for test: each stage contains human data, and the previous model data
+            temp = {
+                'text': [],
+                'label': []
+            }
+            prev_test_data = data['test'][i - 1] # i > 0
+            temp = copy.deepcopy(prev_test_data)
+            # add current stage model data, i.e. a new class
+            for d in test_data[order[i]]:
+                temp['text'].append(d['text'])
+                temp['label'].append(i + 1)
+
+            data['test'].append(temp)
+
+    return data
+
+def load_incremental(order, category):
+    saved_data_path = f"data/{category}_incremental_data.json"
+    if not os.path.exists("data"):
+        os.makedirs("data")
+    if not os.path.exists(saved_data_path):
+        data = prepare_incremental(order, category, seed=3407)
         with open(saved_data_path, 'w') as f:
             json.dump(data, f)
     else:
