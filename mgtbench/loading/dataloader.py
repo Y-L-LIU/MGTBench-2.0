a9bb0a74a98d57dd49eb58db9a8a4c334d9325d7
cooper <12110517@mail.sustech.edu.cn>
2024-11-07 03:42:47 +0000

    update dataloader

--- mgtbench/loading/dataloader.py
+++ mgtbench/loading/dataloader.py
@@ -387,18 +387,23 @@ def load_attribution(category):
 
 def prepare_incremental(order: list, category='Art', seed=0):
     '''
-    Prepare incremental data for the given category containing 6 models and human data,
-    the order of each model is given by the order list
-    order: ['Moonshot', 'gpt35', 'Llama3'] for example
+    Prepare incremental data for the given category containing specified models and human data,
+    with the order of each model set given by the list of lists in "order".
+    Example: order = [['Moonshot'], ['gpt35', 'Llama3']]
     '''
     setup_seed(seed)
-    # human data
-    subject_human_data = datasets.load_dataset("AITextDetect/AI_Polish_clean", trust_remote_code=True, name='Human', split=category, cache_dir='/data1/zzy/cache/huggingface')
+    
+    # Load human data
+    subject_human_data = datasets.load_dataset(
+        "AITextDetect/AI_Polish_clean", trust_remote_code=True, 
+        name='Human', split=category
+    )
 
-    # models data
+    # Load models data
     model_data = {}
+    all_models = [model for group in order for model in group]
     with ThreadPoolExecutor() as executor:
-        futures = {executor.submit(download_data, m, category, '/data1/zzy/cache/huggingface'): m for m in MODELS}
+        futures = {executor.submit(download_data, m, category): m for m in all_models}
         for future in futures:
             model_name = futures[future]
             try:
@@ -406,94 +411,89 @@ def prepare_incremental(order: list, category='Art', seed=0):
             except Exception as e:
                 print(f"Failed to load {model_name}: {e}")
 
-    # TODO: now contrain the length of data to be the same
-    min_len = int(1e9)
-    for m in MODELS:
-        min_len = min(min_len, len(model_data[m]))
+    # Determine the data length for each round
+    if len(order[0]) == 1:
+        first_round_len = len(model_data[order[0][0]])
+    else:
+        first_round_len = min(len(model_data[model]) for model in order[0])
 
-    for m in MODELS:
-        model_data[m] = model_data[m].shuffle().select(range(min_len))
+    # Limit the human sample to the first round length
+    human_sample = subject_human_data.shuffle().select(range(first_round_len))
 
-    human_sample = subject_human_data.shuffle().select(range(min_len))
+    # Prepare model data length according to the first round's length
+    for m in model_data:
+        if len(model_data[m]) > first_round_len:
+            model_data[m] = model_data[m].shuffle().select(range(first_round_len))
 
+    # Train/test split
     split = 0.8
     train_data = {}
     test_data = {}
-    for m in MODELS:
-        train_data[m] = model_data[m].select(range(int(min_len * split)))
-        test_data[m] = model_data[m].select(range(int(min_len * split), min_len))
+    for m in all_models:
+        available_len = len(model_data[m])
+        current_len = min(available_len, first_round_len)
+        train_data[m] = model_data[m].select(range(int(current_len * split)))
+        test_data[m] = model_data[m].select(range(int(current_len * split), current_len))
 
-    train_data['Human'] = human_sample.select(range(int(min_len * split)))
-    test_data['Human'] = human_sample.select(range(int(min_len * split), min_len))
+    train_data['Human'] = human_sample.select(range(int(first_round_len * split)))
+    test_data['Human'] = human_sample.select(range(int(first_round_len * split), first_round_len))
 
-    # label_mapping = {'Human': 0, 'Moonshot': 1, 'gpt35': 2, 'Mixtral': 3, 'Llama3': 4, 'gpt-4omini': 5}
-    num_class = len(order)
-
-    # a list of data, len(train) == len(order), for each round of incremental learning
+    # Incremental data structure
     data = {
         'train': [],
         'test': []
     }
-    for i in range(num_class):
+    for i, group in enumerate(order):
+        # Train data: start with human data in the first round, add models as per the order list
+        temp_train = {
+            'text': [],
+            'label': []
+        }
         if i == 0:
-            # for train data, first data should contain shuffled human AND one model data
-            this_model_data = train_data[order[i]]
-            temp = {
-                'text': [],
-                'label': []
-            }
-            data_list = []
-            for d in this_model_data:
-                data_list.append({'text': d['text'], 'label': i + 1})
-            # TODO: here all human data is added to the first data, maybe we can add human data to each stage (as mitigation)
             for d in train_data['Human']:
-                data_list.append({'text': d['text'], 'label': 0})
+                temp_train['text'].append(d['text'])
+                temp_train['label'].append(0)  # Human label
 
-            random.shuffle(data_list)
-            for d in data_list:
-                temp['text'].append(d['text'])
-                temp['label'].append(d['label'])
+        for j, model_name in enumerate(group):
+            model_len = min(len(train_data[model_name]), first_round_len) if i > 0 else len(train_data[model_name])
+            model_train_data = train_data[model_name].select(range(model_len))
+            
+            for d in model_train_data:
+                temp_train['text'].append(d['text'])
+                temp_train['label'].append(j + 1 + sum(len(g) for g in order[:i]))  # Model-specific label
 
-            data['train'].append(temp)
-            # for test data: no need to shuffle
-            temp = {
-                'text': [],
-                'label': []
-            }
-            for d in test_data[order[i]]:
-                temp['text'].append(d['text'])
-                temp['label'].append(i + 1)
+        if len(group) > 1 or i == 0:
+            combined_data = list(zip(temp_train['text'], temp_train['label']))
+            random.shuffle(combined_data)  # Shuffle when there are multiple models
+            temp_train['text'], temp_train['label'] = zip(*combined_data)
+        
+        data['train'].append(temp_train)
+
+        # Test data: include human data initially, add current and past models
+        temp_test = {
+            'text': [],
+            'label': []
+        }
+        if i == 0:
             for d in test_data['Human']:
-                temp['text'].append(d['text'])
-                temp['label'].append(0)
+                temp_test['text'].append(d['text'])
+                temp_test['label'].append(0)  # Human label
 
-            data['test'].append(temp)
-        else:
-            # for train: the rest of the data contains only one model data
-            this_model_data = train_data[order[i]]
-            temp = {
-                'text': [],
-                'label': []
-            }
-            for d in this_model_data:
-                temp['text'].append(d['text'])
-                temp['label'].append(i + 1)
+        for j, model_name in enumerate(group):
+            model_len = min(len(test_data[model_name]), first_round_len) if i > 0 else len(test_data[model_name])
+            model_test_data = test_data[model_name].select(range(model_len))
+            
+            for d in model_test_data:
+                temp_test['text'].append(d['text'])
+                temp_test['label'].append(j + 1 + sum(len(g) for g in order[:i]))  # Model-specific label
 
-            data['train'].append(temp)
+        if i > 0:
+            # Include previous test data in the current round
+            prev_test_data = data['test'][i - 1]
+            temp_test['text'].extend(prev_test_data['text'])
+            temp_test['label'].extend(prev_test_data['label'])
 
-            # for test: each stage contains human data, and the previous model data
-            temp = {
-                'text': [],
-                'label': []
-            }
-            prev_test_data = data['test'][i - 1] # i > 0
-            temp = copy.deepcopy(prev_test_data)
-            # add current stage model data, i.e. a new class
-            for d in test_data[order[i]]:
-                temp['text'].append(d['text'])
-                temp['label'].append(i + 1)
-
-            data['test'].append(temp)
+        data['test'].append(temp_test)
 
     return data
 
